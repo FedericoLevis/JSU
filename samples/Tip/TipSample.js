ebf6f858d2b7974f7e95edc3108196128480b1d2
Federico Levis <flevis2011@gmail.com>
2016-07-19 12:34:38 +0200

    adding files

--- samples/Tip/TipSample.js
+++ samples/Tip/TipSample.js
@@ -17,19 +17,7 @@ var JSU_TIP_README='<table class="tip" BORDER="2" cellspacing="0" cellpadding="2
 var JSU_TIP_INFO=JSU_TIP_HTML;
 
 
-var TIP_FIXED_SAMPLE='<table class="tip" BORDER="2" cellspacing="0" cellpadding="2" width="450">' +
-'<tr><td class="tipl">Open/Close <b>Fixed Tip</b>:<ul>' +
-' <li><b>Open</b> Tip by clicking the <b>Down Arrow</b></li>' +
-' <li><b>Close</b> Tip with <b>ESC</b>, or by clicking the <b>Up Arrow</b>, or the <b>X</b> in the TopRight of the Tip, or the Optional <b>Close</b> Button if present</li>' +
-'</ul>' +
-'You can <b>Interact with Fixed Tip using the mouse</b>. For example:<ul>' +
-'<li>Click on a URL: <a class="tipLink" href="https://www.google.it" target="_blank">Google</a></li>' +
-'<li>Select Tip Text (Copy/paste)</li>' +
-'<li>Click on Buttons <input type="button" value="Click Me" onclick="showInfo(\'Hello\');">  </li>' +
-'</ul>' + 
-'In the Tip you can display whatever HTML object, also <b>GIF</b> <BR/>' +
-'<div align="center"><img height="200px" src="../../images/about/JSUDancing.gif" /></div>' +
-'</td></tr></table>';
+
 
 var TIP_VIDEO_SAMPLE=	'<iframe width="600" height="500" src="https://www.youtube.com/embed/SuYxv1z1BMg?version=3&vq=hd720&autoplay=1" frameborder="0" allowfullscreen></iframe>'; 
 
@@ -49,6 +37,27 @@ function jsu_loaded(){
   // jslog_init(JSLOG_LEV);
 		
 }
+
+
+
+
+function sample2Fixed(event, iMaxHeight,bCloseBtn){
+	var szTip='<table class="tip" BORDER="2" cellspacing="0" cellpadding="2" width="450">' +
+	'<tr><td class="tipl">Open/Close <b>Fixed Tip</b>:<ul>' +
+	' <li><b>Open</b> Tip by clicking the <b>Down Arrow</b></li>' +
+	' <li><b>Close</b> Tip with <b>ESC</b>, or by clicking the <b>Up Arrow</b>, or the <b>X</b> in the TopRight of the Tip, or the Optional <b>Close</b> Button if present</li>' +
+	'</ul>' +
+	'You can <b>Interact with Fixed Tip using the mouse</b>. For example:<ul>' +
+	'<li>Click on a URL: <a class="tipLink" href="https://www.google.it" target="_blank">Google</a></li>' +
+	'<li>Select Tip Text (Copy/paste)</li>' +
+	'<li>Click on Buttons <input type="button" value="Click Me" onclick="showInfo(\'Hello\');">  </li>' +
+	'</ul>' + 
+	'In the Tip you can display whatever HTML object, also <b>GIF</b> <BR/>' +
+	'<div align="center"><img height="200px" src="' + JSU_PATH_ABOUT_IMG + 'JSUDancing.gif" /></div>' +
+	'</td></tr></table>';	
+	TipFixedClicked(szTip,event,{szTitle:'Fixed Tip Sample',iMaxHeight:iMaxHeight,bCloseBtn:bCloseBtn});
+}
+
 
 
 //===================================================================================================
