38432b5f5638240870ca77ac3d2559d8abf5cf72
Federico Levis <flevis2011@gmail.com>
2016-07-31 21:56:32 +0200

    adding files

--- samples/AllSamples.js
+++ samples/AllSamples.js
@@ -5,4 +5,4 @@
 /*  http://javascriptobfuscator.com                                        */
 /*                                                                         */
 /***************************************************************************/
-var _$_f7ad=["<b>Sort Feature: DAFARE</b>"];var JSLOG_LEV=0;var TIP_SORT_FEATURE=_$_f7ad[0];function jsu_loaded(){}function onclickSortFeature(I){TipFixedClicked(TIP_SORT_FEATURE,I)}
+var _$_d310=["<b>Sort Feature: DAFARE</b>"];var JSLOG_LEV=0;var TIP_SORT_FEATURE=_$_d310[0];function jsu_loaded(){}function onclickSortFeature(I){TipFixedClicked(TIP_SORT_FEATURE,I)}
