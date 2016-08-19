8f1ff4d6f0a60749f339e97d76bb0c5608581307
Federico Levis <flevis2011@gmail.com>
2016-08-20 01:09:51 +0200

    adding files

--- samples/about/about.js
+++ samples/about/about.js
@@ -953,7 +953,8 @@ function manage_par_opt(){
 		  var iframeEl = window.parent.document.getElementById ('iframe' + szParDoc);				
 			var trEl = getElementById2("tr_sample_" + szParDoc, false);
 			if (trEl){
-				iframeEl.height = trEl.clientHeight + 40; 
+				 // Set new height
+				 iframeEl.height = trEl.clientHeight + 40; 
 			}
 			// now we show the iframe that was hidden
 			elementShow (iframeEl,true);
