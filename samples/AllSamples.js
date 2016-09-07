4089362f46ebcf67e55ff4bd5796663069940500
Federico Levis <flevis2011@gmail.com>
2016-09-08 00:02:33 +0200

    adding files

--- samples/AllSamples.js
+++ samples/AllSamples.js
@@ -5,4 +5,4 @@
 /*  http://javascriptobfuscator.com                                        */
 /*                                                                         */
 /***************************************************************************/
-var _$_ff17=["<b>Sort Feature: DAFARE</b>","videoOpt","disabled"];var JSLOG_LEV=0;var TIP_SORT_FEATURE=_$_ff17[0];function jsu_loaded(){if(isIE()|| isFirefox()){var bd=getElementById2(_$_ff17[1]);bd[_$_ff17[2]]= false};initSampleCmn()}function onclickSortFeature(a){TipFix(TIP_SORT_FEATURE,a)}
+var _$_264b=["<b>Sort Feature: DAFARE</b>","videoOpt","disabled"];var JSLOG_LEV=0;var TIP_SORT_FEATURE=_$_264b[0];function jsu_loaded(){if(isIE()|| isFirefox()){var bd=getElementById2(_$_264b[1]);bd[_$_264b[2]]= false};initSampleCmn()}function onclickSortFeature(a){TipFix(TIP_SORT_FEATURE,a)}
